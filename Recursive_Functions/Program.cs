9c311bd4c4547bd53b9709ee587d8260570ff49e
Ilya Silkin <zerolar@gmail.com>
2021-04-27 21:05:42 +0300

    Added Echo method

--- Recursive_Functions/Program.cs
+++ Recursive_Functions/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace Recursive_Functions
+class MainClass
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Напишите что-то");
+        var str = Console.ReadLine();
+
+        Console.WriteLine("Укажите глубину эха");
+        var deep = int.Parse(Console.ReadLine());
+
+        Echo(str, deep);
+
+        Console.ReadKey();
+    }
+
+    static void Echo(string saidworld, int deep)
+    {
+        var modif = saidworld;
+        Console.WriteLine("..." + modif);
+
+        if (modif.Length > 2)
         {
-            Console.WriteLine("Hello World!");
+            modif = modif.Remove(0, 2);
         }
+
+        if (deep > 1)
+        {
+            Echo(modif, deep - 1);
+        }
+
     }
 }
